e213882a182f60479851bb482aeae6b6775bd06e
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-06-19 20:03:15 +0530

    1

---
+++ Day_8.py
@@ -0,0 +1,12 @@
+'''def greet():
+    print("Hello")
+    print("How are you")
+    print("Where do you live")
+greet()'''
+#function that allow for the input
+def greet1(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}")
+    print("Where do you live")
+greet1("Arya" )
+
