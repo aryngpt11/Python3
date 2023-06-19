b020b0a5d64d757fe55f0577d78e59ce0fe3a979
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-06-19 20:28:19 +0530

    3types_function

--- Day_8.py
+++ Day_8.py
@@ -4,9 +4,24 @@
     print("Where do you live")
 greet()'''
 #function that allow for the input
-def greet1(name):
+
+'''def greet1(name):
     print(f"Hello {name}")
     print(f"How are you {name}")
     print("Where do you live")
-greet1("Arya" )
+greet1("Arya" )'''
 
+
+#function with more than one input
+'''def greet_with(name, address):
+    print(f"Hello {name}")
+    print(f"what u like in {address} ")
+greet_with("Riya","Delhi")'''
+
+#Keyword Argument
+
+def greet_with(name, address):
+    print(f"Hello {name}")
+    print(f"what u like in {address} ")
+#greet_with(name="Riya",address="Delhi")
+greet_with(address="Delhi",name="Riya")
